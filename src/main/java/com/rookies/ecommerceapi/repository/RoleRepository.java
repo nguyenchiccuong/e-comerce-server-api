86f8035379dc170238cbc57ea82954bb690c0f82
CuongNC <you@example.com>
2021-07-05 02:29:20 +0700

    add function to role and user repository use for security

--- src/main/java/com/rookies/ecommerceapi/repository/RoleRepository.java
+++ src/main/java/com/rookies/ecommerceapi/repository/RoleRepository.java
@@ -3,9 +3,12 @@ package com.rookies.ecommerceapi.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 import com.rookies.ecommerceapi.entity.Role;
+import com.rookies.ecommerceapi.entity.RoleName;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Short>{
-    
+    Optional<Role> findByRoleName(RoleName roleName); 
 }
--- src/main/java/com/rookies/ecommerceapi/repository/UserRepository.java
+++ src/main/java/com/rookies/ecommerceapi/repository/UserRepository.java
@@ -2,10 +2,13 @@ package com.rookies.ecommerceapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 import com.rookies.ecommerceapi.entity.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long>{
-    
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
 }

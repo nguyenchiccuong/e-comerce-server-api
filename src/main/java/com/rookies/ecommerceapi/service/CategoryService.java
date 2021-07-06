590e68977de30b5b66ee08d05088ad05bbfcb4c5
CuongNC <you@example.com>
2021-07-07 00:27:29 +0700

    add find parent category and find sub category in service

--- src/main/java/com/rookies/ecommerceapi/service/CategoryService.java
+++ src/main/java/com/rookies/ecommerceapi/service/CategoryService.java
@@ -1,5 +1,12 @@
 package com.rookies.ecommerceapi.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.rookies.ecommerceapi.entity.Category;
+
 public interface CategoryService {
-    
+    public List<Category> retrieveParentCategory();
+
+    public List<Category> retrieveSubCategory(Integer parentId);
 }
--- src/main/java/com/rookies/ecommerceapi/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/rookies/ecommerceapi/service/impl/CategoryServiceImpl.java
@@ -1,10 +1,31 @@
 package com.rookies.ecommerceapi.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
+import com.rookies.ecommerceapi.entity.Category;
+import com.rookies.ecommerceapi.repository.CategoryRepository;
 import com.rookies.ecommerceapi.service.CategoryService;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    @Autowired
+    public CategoryServiceImpl(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public List<Category> retrieveParentCategory() {
+        return categoryRepository.findByCategoryIsNull();
+    }
+
+    @Override
+    public List<Category> retrieveSubCategory(Integer parentId) {
+        return categoryRepository.findSubCategoryByParentCategoryId(parentId);
+    }
 
 }

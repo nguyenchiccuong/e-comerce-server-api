f8a0a2becc11d343835296c487866f88a9ceccd8
CuongNC <you@example.com>
2021-07-07 00:28:10 +0700

    add find parent category and find sub category in controller

---
+++ src/main/java/com/rookies/ecommerceapi/restcontroller/CategoryController.java
@@ -0,0 +1,43 @@
+package com.rookies.ecommerceapi.restcontroller;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rookies.ecommerceapi.dto.CategoryDto;
+import com.rookies.ecommerceapi.entity.Category;
+import com.rookies.ecommerceapi.service.CategoryService;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/v1/public/category")
+public class CategoryController {
+    private final CategoryService categoryService;
+
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public CategoryController(CategoryService categoryService, ModelMapper modelMapper) {
+        this.categoryService = categoryService;
+        this.modelMapper = modelMapper;
+    }
+
+    @GetMapping("/parent")
+    public List<CategoryDto> retrieveParentCategory() {
+        return categoryService.retrieveParentCategory().stream()
+                .map(category -> modelMapper.map(category, CategoryDto.class)).collect(Collectors.toList());
+    }
+
+    @GetMapping("/sub/{parentId}")
+    public List<CategoryDto> retrieveSubCategory(@PathVariable("parentId") Integer parentId) {
+        return categoryService.retrieveSubCategory(parentId).stream()
+                .map(category -> modelMapper.map(category, CategoryDto.class)).collect(Collectors.toList());
+    }
+}

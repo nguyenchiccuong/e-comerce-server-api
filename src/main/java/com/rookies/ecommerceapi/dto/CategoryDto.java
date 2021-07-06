cc29bca7041c45888cb2297a476597612f383fb8
CuongNC <you@example.com>
2021-07-06 23:21:56 +0700

    create CategoryDto

---
+++ src/main/java/com/rookies/ecommerceapi/dto/CategoryDto.java
@@ -0,0 +1,24 @@
+package com.rookies.ecommerceapi.dto;
+
+public class CategoryDto {
+    private Integer id;
+
+    private String categoryName;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+}

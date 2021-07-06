5ac863baef1ba68b84ed3d892ead860d2c9ddfd7
CuongNC <you@example.com>
2021-07-07 00:26:55 +0700

    add find parent category and find sub category in repository

--- src/main/java/com/rookies/ecommerceapi/repository/CategoryRepository.java
+++ src/main/java/com/rookies/ecommerceapi/repository/CategoryRepository.java
@@ -1,11 +1,22 @@
 package com.rookies.ecommerceapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 import com.rookies.ecommerceapi.entity.Category;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<Category, Integer>{
-    
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+    // @Query("FROM Category c WHERE c.category is null")
+    // List<Category> findParentCategory();
+
+    List<Category> findByCategoryIsNull();
+
+    @Query("FROM Category c WHERE c.category.id =?1")
+    List<Category> findSubCategoryByParentCategoryId(Integer parentId);
+
 }

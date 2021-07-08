8ff56d8db863262d87fbb0370f370564ef3d4780
CuongNC <you@example.com>
2021-07-09 02:14:46 +0700

    add product and product detail dto

---
+++ src/main/java/com/rookies/ecommerceapi/dto/ProductDetailDto.java
@@ -0,0 +1,29 @@
+package com.rookies.ecommerceapi.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ProductDetailDto {
+    private Long id;
+
+    // @NotNull
+    private long productId;
+
+    @NotBlank // @NotNull
+    private String color;
+
+    @NotNull
+    private Integer quantity;
+
+    @NotNull
+    private Long price;
+
+}
---
+++ src/main/java/com/rookies/ecommerceapi/dto/ProductDto.java
@@ -0,0 +1,56 @@
+package com.rookies.ecommerceapi.dto;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ProductDto {
+    private Long id;
+
+    @NotBlank // @NotNull
+    private String productName;
+
+    @NotNull
+    private Integer categoryId;
+    private CategoryDto category;
+
+    private String model;
+
+    @NotNull
+    private Integer brandId;
+    private BrandDto brand;
+
+    @NotNull
+    private Integer originId;
+    private OriginDto origin;
+
+    private String standard;
+
+    private String size;
+
+    private Float weight;
+
+    private String material;
+
+    private String description;
+
+    private Short warranty;
+
+    private Short img;
+
+    // @NotNull
+    private LocalDateTime createDate;
+
+    private LocalDateTime updateDate;
+
+    private Collection<ProductDetailDto> productDetails;
+}
